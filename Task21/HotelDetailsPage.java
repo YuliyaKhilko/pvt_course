8bb630d612b545395e0433b316b746f3f2e78bf3
Yuliya Khilko <ylkhilko@gmail.com>
2019-03-09 03:13:58 +0300

    add task21

---
+++ Task21/HotelDetailsPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HotelDetailsPage {
+
+	@FindBy(xpath = ".//div[@class=\"bui-review-score c-score bui-review-score--end\"]/div[@class=\"bui-review-score__badge\"]")
+	private WebElement rating;
+
+	public HotelDetailsPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public double getRating() {
+		return Double.valueOf(rating.getText());
+	}
+}
