c18d556d4953be680672a7f9272f96592702470c
Yuliya Khilko <ylkhilko@gmail.com>
2019-03-25 00:15:39 +0300

    Add task25

---
+++ Task25/TestData.java
@@ -0,0 +1,68 @@
+package core;
+
+public class TestData {
+	private String login;
+	private String password;
+	private String email;
+	private String emailSubject;
+	private String emailBody;
+	private String groupName;
+	private String name;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getEmailSubject() {
+		return emailSubject;
+	}
+
+	public String getEmailBody() {
+		return emailBody;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setEmailSubject(String emailSubject) {
+		this.emailSubject = emailSubject;
+	}
+
+	public void setEmailBody(String emailBody) {
+		this.emailBody = emailBody;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
